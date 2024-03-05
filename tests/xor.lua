7f8bcebca99263e8f953517a28703b1fe12fe1f7
Christopher Moore <christopher.e.moore@gmail.com>
2024-03-05 10:23:29 +0700

    more

--- tests/xor.lua
+++ tests/xor.lua
@@ -3,8 +3,8 @@
 local table = require 'ext.table'
 local range = require 'ext.range'
 local gnuplot = require 'gnuplot'
---local ANN = require 'neuralnet.ann'
-local ANN = require 'neuralnet.ann-ffi'
+local ANN = require 'neuralnet.ann'
+--local ANN = require 'neuralnet.ann-ffi'
 
 --math.randomseed(os.time())
 math.randomseed(1)
@@ -12,7 +12,28 @@ math.randomseed(1)
 -- a xor b == (a and not b) or (not a or b)
 -- which means it'll take union of two hulls / hyperplane-collections to classify
 -- which means it'll need a hidden layer and biases to combine them
+-- [[
 local nn = ANN(2,2,1)
+--]]
+--[[ logical-AND
+local nn = ANN(2,1)
+--]]
+
+--[[
+local ident = function(x,y) return x end
+local one = function(x,y) return 1 end
+nn.activation = ident
+nn.activationDeriv = one
+--]]
+-- [[
+local hi = .9
+local lo = -.9
+--]]
+--[[
+local hi = 1
+local lo = -1
+--]]
+nn.dt = .1
 
 local results = range(5):map(function() return table() end)
 
@@ -20,9 +41,14 @@ local results = range(5):map(function() return table() end)
 for i=1,100000 do
 	local a = math.random(2) == 2
 	local b = math.random(2) == 2
+	-- [[ xor	
 	local c = a ~= b	--xor problem.  requires two layers to solve:  (a & ~b) | (~a & b) ... or ... (a | b) & (~a | ~b) ... either is 2 operations deep <=> two layers
-	nn.input[1] = a and .9 or -.9
-	nn.input[2] = b and .9 or -.9
+	--]]
+	--[[ and
+	local c = a and b
+	--]]
+	nn.input[1] = a and hi or lo
+	nn.input[2] = b and hi or lo
 	--print('input', nn.input)
 	--print('input weight', nn.w[1])
 	--print('mid weight', nn.w[2])
@@ -31,7 +57,7 @@ for i=1,100000 do
 	--print('mid', nn.x[2])
 	--print('net out', nn.net[2])
 	--print('out', nn.x[3])
-	nn.desired[1] = c and .9 or -.9
+	nn.desired[1] = c and hi or lo
 	--print('desired', nn.desired)
 	local err = nn:calcError()
 	results[1]:insert(nn.input[1])
@@ -41,12 +67,17 @@ for i=1,100000 do
 	results[5]:insert(err)
 	
 	--print('de/dy', nn.xErr[3])
-	nn:backPropagate(.1)
+	nn:backPropagate()
 	--print('de/dnet y', nn.netErr[2])
 	--print('de/dmid', nn.xErr[2])
 	--print('de/dnet mid', nn.netErr[1])
 	--print('de/dx', nn.xErr[1])
 	--print()
+end
+
+for i,w in ipairs(nn.w) do
+	print('layer '..i..':')
+	print(w)
 end
 
 gnuplot{

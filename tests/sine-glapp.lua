89f04a1c7379cfc9900fca3bcfac28c4c8ed160e
thenumbernine <christopher.e.moore@gmail.com>
2024-08-25 21:18:02 -0700

    now the default

--- tests/sine-glapp.lua
+++ tests/sine-glapp.lua
@@ -6,7 +6,7 @@ local gl = require 'gl'
 local ANN = require 'neuralnet.ann'
 
 local App = require 'imguiapp.withorbit'()
-
+App.viewUseGLMatrixMode = true
 local f = math.sin
 local xmin = 0
 local xmax = 2 * math.pi
